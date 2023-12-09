efe6b3c136677bdae4d5f66db0ef2f25e56e2e68
vinialves2020 <vinicius.livramentofga@gmail.com>
2023-12-09 12:14:59 -0300

    asasas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,228 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { FireDb } from './firebaseConfig';
+import { collection, addDoc } from "firebase/firestore"; 
 
-function App() {
+const FormularioWrapper = styled.form`
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 10px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 15px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+const Button = styled.button`
+  background-color: #4caf50;
+  color: white;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+`;
+
+const App = () => {
+  const[results,setResults] = useState([])
+  const [formulario, setFormulario] = useState({
+    Nome: '',
+    NomeAlternativos: '',
+    Cor:'',
+    URL:'',
+    Descricao: '',
+    Colheita: '',
+    Solo: '',
+    Luz: '',
+    Rega: '',
+    Regiao: '',
+    Detalhes: '',
+    PreparacaoDoSolo: '',
+    Plantio: '',
+    RegaDetalhes: '',
+    Adubacao: '',
+    ProtecaoDePragas: '',
+    Paciencia: '',
+    ColheitaDetalhes: ''
+  });
+
+  const handleChange = (campo, valor) => {
+    setFormulario({ ...formulario, [campo]: valor });
+    setResults({ ...formulario, [campo]: valor });
+  };
+
+  const handleSubmit =  async (event) => {
+    event.preventDefault();
+    await addDoc(collection(FireDb, "Plantas"), {
+    Nome: formulario.Nome,
+    NomeAlternativos: formulario.NomeAlternativos,
+    Cor:formulario.Cor,
+    URL:formulario.URL,
+    Descricao: formulario.Descricao,
+    Colheita: formulario.Colheita,
+    Solo: formulario.Solo,
+    Luz: formulario.Luz,
+    Rega: formulario.Rega,
+    Regiao: formulario.Regiao,
+    Detalhes: formulario.Detalhes,
+    PreparacaoDoSolo: formulario.PreparacaoDoSolo,
+    Plantio: formulario.Plantio,
+    RegaDetalhes: formulario.RegaDetalhes,
+    Adubacao: formulario.Adubacao,
+    ProtecaoDePragas: formulario.ProtecaoDePragas,
+    Paciencia: formulario.Paciencia,
+    ColheitaDetalhes: formulario.ColheitaDetalhes,
+    });
+    console.log(formulario);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <FormularioWrapper onSubmit={handleSubmit}>
+      <Label>
+        Nome:
+        <Input
+          type="text"
+          value={formulario.Nome}
+          onChange={(e) => handleChange('Nome', e.target.value)}
+        />
+      </Label>
+      <Label>
+        Nome Alternativos:
+        <Input
+          type="text"
+          value={formulario.NomeAlternativos}
+          onChange={(e) => handleChange('NomeAlternativos', e.target.value)}
+        />
+      </Label>
+      <Label>
+      Cor:
+        <Input
+          type="text"
+          value={formulario.Cor}
+          onChange={(e) => handleChange('Cor', e.target.value)}
+        />
+      </Label><Label>
+      URL:
+        <Input
+          type="text"
+          value={formulario.URL}
+          onChange={(e) => handleChange('URL', e.target.value)}
+        />
+      </Label><Label>
+      Descrição:
+        <Input
+          type="text"
+          value={formulario.Descricao}
+          onChange={(e) => handleChange('Descricao', e.target.value)}
+        />
+      </Label><Label>
+      Colheita:
+        <Input
+          type="text"
+          value={formulario.Colheita}
+          onChange={(e) => handleChange('Colheita', e.target.value)}
+        />
+      </Label><Label>
+      Solo:
+        <Input
+          type="text"
+          value={formulario.Solo}
+          onChange={(e) => handleChange('Solo', e.target.value)}
+        />
+      </Label><Label>
+      Luz:
+        <Input
+          type="text"
+          value={formulario.Luz}
+          onChange={(e) => handleChange('Luz', e.target.value)}
+        />
+      </Label><Label>
+      Rega:
+        <Input
+          type="text"
+          value={formulario.Rega}
+          onChange={(e) => handleChange('Rega', e.target.value)}
+        />
+      </Label><Label>
+      Região:
+        <Input
+          type="text"
+          value={formulario.Regiao}
+          onChange={(e) => handleChange('Regiao', e.target.value)}
+        />
+      </Label><Label>
+      Detalhes:
+        <Input
+          type="text"
+          value={formulario.Detalhes}
+          onChange={(e) => handleChange('Detalhes', e.target.value)}
+        />
+      </Label><Label>
+      Preparação Do Solo:
+        <Input
+          type="text"
+          value={formulario.PreparacaoDoSolo}
+          onChange={(e) => handleChange('PreparacaoDoSolo', e.target.value)}
+        />
+      </Label><Label>
+      Plantio:
+        <Input
+          type="text"
+          value={formulario.Plantio}
+          onChange={(e) => handleChange('Plantio', e.target.value)}
+        />
+      </Label><Label>
+      Rega Detalhes:
+        <Input
+          type="text"
+          value={formulario.RegaDetalhes}
+          onChange={(e) => handleChange('RegaDetalhes', e.target.value)}
+        />
+      </Label><Label>
+      Adubação:
+        <Input
+          type="text"
+          value={formulario.Adubacao}
+          onChange={(e) => handleChange('Adubacao', e.target.value)}
+        />
+      </Label><Label>
+      Protecao De Pragas:
+        <Input
+          type="text"
+          value={formulario.ProtecaoDePragas}
+          onChange={(e) => handleChange('ProtecaoDePragas', e.target.value)}
+        />
+      </Label><Label>
+      Paciencia:
+        <Input
+          type="text"
+          value={formulario.Paciencia}
+          onChange={(e) => handleChange('Paciencia', e.target.value)}
+        />
+      </Label>
+      <Label>
+      ColheitaDetalhes:
+        <Input
+          type="text"
+          value={formulario.ColheitaDetalhes}
+          onChange={(e) => handleChange('ColheitaDetalhes', e.target.value)}
+        />
+      </Label>
+      <Button type="submit">Enviar</Button>
+    </FormularioWrapper>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAn8UsInKaDpqnxoGOi5CTsTouwQAJ3FB0",
+  authDomain: "ds2023-74d02.firebaseapp.com",
+  projectId: "ds2023-74d02",
+  storageBucket: "ds2023-74d02.appspot.com",
+  messagingSenderId: "61322629826",
+  appId: "1:61322629826:web:2ff9c124b676a793a8e15b",
+  measurementId: "G-TZ1TKBDR30"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const FireDb = getFirestore(app);
